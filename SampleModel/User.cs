cf5a7397f4e7bcf07db3c43864002805460e1328
Jesper Niedermann <jn@uvdata.dk>
2018-04-26 09:54:22 +0200

    Sandkasse thingy

---
+++ SampleModel/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SampleModel
+{
+    public class User
+    {
+        public string Name { get; set; } = "Tom";
+        public bool IsAdmin { get; set; } = true;
+        public int Age { get; set; } = 33;
+        public DateTime Birthday { get; set; } = DateTime.Today;
+        public decimal Salary { get; set; }
+        public bool Turned { get; set; }
+        public string FrontClass => !Turned ? "front card cardfront" : "frontturned card cardback";
+        public string BackClass => !Turned ? "back card cardback" : "backturned card cardfront";
+    }
+}
